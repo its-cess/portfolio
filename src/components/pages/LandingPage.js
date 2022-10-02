35c7f6828f64ced6c2cadfc927127648939099d5
Cecily Toro <torocecily@gmail.com>
2022-10-02 11:21:28 -0400

    basic landing page layout, styles, and animations

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,3 +1,22 @@
+import "./landing-page.styles.scss";
+
 export default function LandingPage() {
-  return <h1>LandingPage</h1>;
+  return (
+    <div className="landing-page-container">
+      <h1 className="banner">Welcome!</h1>
+      <p className="introduction">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Accumsan lacus vel
+        facilisis volutpat est velit egestas dui. Blandit massa enim nec dui
+        nunc mattis. Dictumst vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim cras tincidunt. Cras adipiscing enim eu turpis
+        egestas pretium. Non enim praesent elementum facilisis leo. Vestibulum
+        lorem sed risus ultricies tristique nulla aliquet enim tortor. Nibh cras
+        pulvinar mattis nunc. Id diam vel quam elementum pulvinar etiam non.
+        Proin libero nunc consequat interdum varius. Et molestie ac feugiat sed
+        lectus vestibulum mattis. In nulla posuere sollicitudin aliquam ultrices
+        sagittis orci a.
+      </p>
+    </div>
+  );
 }
