d7f212fcb2523ebe28037f6b24c4b3496c39a055
Cecily Toro <torocecily@gmail.com>
2022-10-03 13:28:36 -0400

    add rest of work experience to data file

--- src/ticketData.js
+++ src/ticketData.js
@@ -1,10 +1,10 @@
 let tickets = [
   {
     id: 1,
-    title: "Brand Page Updates",
-    beforeImage: "/BEFORE-brand-page.png",
-    afterImage: "/brand-page.png",
-
+    title: "Create Project Modal Update",
+    beforeImage: "/BEFORE-project-modal.png",
+    afterImage: "/project-type-toggle.png",
+    metricImage: "/METRIC-client-modal.png",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
     impact:
@@ -16,6 +16,71 @@ let tickets = [
     beforeImage: "/BEFORE-mobile-templates.png",
     afterImage: "/mobile-templates.png",
     metricImage: "/METRIC-mobile-templates.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 3,
+    title: "Proposal Builder Updates",
+    beforeImage: "/BEFORE-proposal-builder.png",
+    afterImage: "/proposal-builder.png",
+    metricImage: "/METRIC-proposal-builder.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 4,
+    title: "Project Status Tag",
+    beforeImage: "/BEFORE-project-status-tag.png",
+    afterImage: "/project-status-dropdown.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 5,
+    title: "Brand Page Updates",
+    beforeImage: "/BEFORE-brand-page.png",
+    afterImage: "/brand-page.png",
+
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 6,
+    title: "Feature Project",
+    beforeImage: "/team-scope-builder.png",
+    afterImage: "/solo-scope-builder.png",
+
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 7,
+    title: "People Page Updates",
+    beforeImage: "/BEFORE-people-page.png",
+    afterImage: "/people-page.png",
+
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
+    impact:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut."
+  },
+  {
+    id: 8,
+    title: "Project Goals Editor",
+    beforeImage: "/BEFORE-client-goals.png",
+    afterImage: "/project-goals-2.png",
+
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida hendrerit lectus. Dignissim cras tincidunt lobortis feugiat vivamus.",
     impact:
