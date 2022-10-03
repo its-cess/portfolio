339bb165d2c1131025b8571d70906a08473e0a41
Cecily Toro <torocecily@gmail.com>
2022-10-03 11:55:35 -0400

    add buttons to project items, update project data

--- src/components/items/ProjectItem.js
+++ src/components/items/ProjectItem.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import Button from "react-bootstrap/Button";
+
 import "./project-item.styles.scss";
 
 export default function ProjectItem(props) {
@@ -8,19 +10,35 @@ export default function ProjectItem(props) {
       <div className="project-item-container">
         <div className="image-title-container">
           <h1 className="project-name">{props.title}</h1>
-          <a href={props.url} target="_blank" rel="noreferrer">
-            <img
-              alt={`${props.title}`}
-              src={process.env.PUBLIC_URL + `/images/${props.image}`}
-              className="thumbnail"
-            />
-          </a>
+          <img
+            alt={`${props.title}`}
+            src={process.env.PUBLIC_URL + `/images/${props.image}`}
+            className="thumbnail"
+          />
         </div>
         <div className="description-container">
           <p className="project-description">{props.description}</p>
           <p>
             <span>{props.using}</span>
           </p>
+          <Button
+            variant="secondary"
+            href={props.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Visit Page
+          </Button>
+          {props.repo && (
+            <Button
+              variant="outline-secondary"
+              href={props.repo}
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub Repository
+            </Button>
+          )}
         </div>
       </div>
     </Fragment>
--- src/projectData.js
+++ src/projectData.js
@@ -15,6 +15,7 @@ let projects = [
     description: "CRUD operations and authentication",
     image: "/blog.png",
     url: "https://www.loom.com/share/a21002c7293e44588904a7b4149f81c9",
+    repo: "https://github.com/its-cess/blog",
     using: "Vue 2, Vue Router, Axios, Headless CMS, Bootstrap"
   },
   {
@@ -24,6 +25,7 @@ let projects = [
       "A rolodex of monsters fetched from an API. Ability to search monsters by name. Built with class components.",
     image: "/monsters-rolodex.png",
     url: "https://its-cess.github.io/react-classes-monsters/",
+    repo: "https://github.com/its-cess/react-classes-monsters",
     using: "React.js"
   },
   {
@@ -33,6 +35,7 @@ let projects = [
       "An application that allows you to add and delete resources. Clicking a resource takes you to the page of the resource.",
     image: "/resource-library.png",
     url: "https://its-cess.github.io/resource-library/",
+    repo: "https://github.com/its-cess/resource-library",
     using: "Vue.js"
   },
   {
@@ -41,6 +44,7 @@ let projects = [
     description: "Can you slay the monster?",
     image: "/monster-slayer.png",
     url: "https://its-cess.github.io/monster-slayer/",
+    repo: "https://github.com/its-cess/monster-slayer",
     using: "Vue.js"
   },
   {
@@ -49,6 +53,7 @@ let projects = [
     description: "All your favorite recipes in one place.",
     image: "/recipes.png",
     url: "https://its-cess.github.io/odin-recipes/recipes/",
+    repo: "https://github.com/its-cess/odin-recipes",
     using: "HTML, CSS, Bootstrap"
   },
   {
@@ -58,6 +63,7 @@ let projects = [
       "An interactive to-do list. Add items to the list and check them off when they're completed!",
     image: "/to-do-list.png",
     url: "https://its-cess.github.io/to-do-list/",
+    repo: "https://github.com/its-cess/to-do-list",
     using: "React"
   },
   {
@@ -66,6 +72,7 @@ let projects = [
     description: "Webpage for a fake startup called Tindog. (Tinder for dogs!)",
     image: "/tindog.png",
     url: "https://its-cess.github.io/tindog/",
+    repo: "https://github.com/its-cess/tindog",
     using: "HTML, CSS, Bootstrap"
   },
   {
@@ -74,6 +81,7 @@ let projects = [
     description: "Play this Simon Says game to test your memory!",
     image: "/simon-game.png",
     url: "https://its-cess.github.io/simon-game/",
+    repo: "https://github.com/its-cess/simon-game",
     using: "HTML, CSS, JavaScript"
   },
   {
@@ -82,6 +90,7 @@ let projects = [
     description: "Play the drums using your keyboard or mouse.",
     image: "/drum-kit.png",
     url: "https://its-cess.github.io/drum-kit/",
+    repo: "https://github.com/its-cess/drum-kit",
     using: "HTML, CSS, JavaScript"
   },
   {
@@ -90,6 +99,7 @@ let projects = [
     description: "Simple and fun dice game!",
     image: "/dicee.png",
     url: "https://its-cess.github.io/dice-game/",
+    repo: "https://github.com/its-cess/dice-game",
     using: "HTML, CSS, JavaScript"
   }
 ];
