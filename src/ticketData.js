49aba572cf7e56f0ffff81590c8cebde2d5775c9
Cecily Toro <torocecily@gmail.com>
2022-10-03 12:04:52 -0400

    add internship screenshot, basic ticket object outline

---
+++ src/ticketData.js
@@ -0,0 +1,13 @@
+let tickets = [
+  {
+    id: 1,
+    title: "",
+    beforeImage: "url",
+    afterImage: "url",
+    metricImage: "url",
+    description: "Description of ticket",
+    impact: "Explains what the metric means"
+  }
+];
+
+export default tickets;
