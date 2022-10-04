927a149ae9e5a4f9fb9c7aca5b8fec600efd8b97
Cecily Toro <torocecily@gmail.com>
2022-10-04 11:01:00 -0400

    add about me copy

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -14,39 +14,52 @@ export default function AboutMe() {
         span="Junior Front End Web Developer"
       />
       <p className="about-me-container">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Id semper risus in
-        hendrerit. Fringilla est ullamcorper eget nulla facilisi etiam dignissim
-        diam. Donec ultrices tincidunt arcu non sodales neque. Ornare quam
-        viverra orci sagittis eu volutpat odio facilisis. Suscipit tellus mauris
-        a diam maecenas. In mollis nunc sed id semper risus. Arcu felis bibendum
-        ut tristique et egestas. Urna nec tincidunt praesent semper. Vel
-        pharetra vel turpis nunc eget. Id donec ultrices tincidunt arcu non
-        sodales. Arcu cursus vitae congue mauris rhoncus aenean vel. At
-        elementum eu facilisis sed odio morbi quis commodo. Nam at lectus urna
-        duis. Libero justo laoreet sit amet. Libero nunc consequat interdum
-        varius sit amet. A erat nam at lectus urna duis convallis. Sit amet nisl
-        suscipit adipiscing bibendum est ultricies integer. Vitae congue eu
-        consequat ac. Pellentesque nec nam aliquam sem et tortor. Justo eget
-        magna fermentum iaculis eu non. Feugiat nisl pretium fusce id velit ut
-        tortor pretium. Fames ac turpis egestas sed tempus urna et pharetra.
-        Enim facilisis gravida neque convallis. At quis risus sed vulputate odio
-        ut enim blandit. Bibendum arcu vitae elementum curabitur vitae. Et netus
-        et malesuada fames ac. Habitant morbi tristique senectus et netus et
-        malesuada fames ac. In aliquam sem fringilla ut morbi. Non consectetur a
-        erat nam at lectus urna. Condimentum mattis pellentesque id nibh tortor
-        id aliquet lectus. In hac habitasse platea dictumst vestibulum rhoncus
-        est pellentesque elit. Turpis egestas integer eget aliquet nibh praesent
-        tristique magna. Varius duis at consectetur lorem donec. At augue eget
-        arcu dictum varius duis at consectetur. Tempus imperdiet nulla malesuada
-        pellentesque elit eget gravida. Tristique magna sit amet purus. Aliquam
-        purus sit amet luctus venenatis lectus. A pellentesque sit amet
-        porttitor eget dolor morbi non arcu. Arcu dictum varius duis at
-        consectetur lorem donec. Ac turpis egestas integer eget aliquet nibh
-        praesent tristique. Tincidunt tortor aliquam nulla facilisi cras. Dui
-        faucibus in ornare quam viverra orci sagittis. Mattis pellentesque id
-        nibh tortor id aliquet lectus proin nibh. Fermentum dui faucibus in
-        ornare quam viverra.
+        I served in the military from 2012-2015, where I was trained as a
+        medical laboratory technician. After separating from the military, I
+        continued to work as a laboratory technician until October 2021 when I
+        decided to switch careers into web development.
+        <br />
+        <br />
+        I worked everyday teaching myself to code. I learned HTML, CSS,
+        JavaScript, jQuery, SASS, and JavaScript frameworks including both Vue
+        and React. I also learned how to use Bootstrap, which prepared me for
+        using other component libraries. Another important skill I learned was
+        version control using Git and GitHub. After 5 months of learning
+        full-time, I landed my first web developmet position as an intern with a
+        startup company, Wethos.
+        <br />
+        <br />
+        During my internship, I built upon the skills I already had, improving
+        them immensely. I gained a deeper knowledge of Vue.js, JavaScript, the
+        use of REST APIs, and SASS. I also learned how to work as part of a
+        development team, which includes being able to jump into exisiting code
+        and figure out what it is doing. I was able to add to my existing
+        knowledge of Git and version control by creating new branches, creating
+        pull requests, and merging branches. I also learned about version
+        control using GitFlow. Part of working in a team also meant working
+        closely with designers to turn Figma and Whimsical files into
+        pixel-perfect web pages. I also had my first experience with unit
+        testing. I created new unit tests using Jest and Vue Test Utils for
+        multiple components, helping to increase the confidence in our site. We
+        were a small team that pushed new features into production weekly, so
+        effective communication and meeting deadlines was critical. Some of the
+        work I did included bug fixes, updating the UI, optimizing performance,
+        creating unit tests, adding Storybook stories for components, and even
+        completing a new feature. To see more in-depth about the work I
+        completed during my internship, please visit the{" "}
+        <a href="/experience" class="inline-link">
+          Work Experience
+        </a>{" "}
+        page.
+        <br />
+        <br />I have found my passion in web development, and love to learn new
+        things. I am constantly looking to better my skills or learn new ones.
+        There is always something new to learn in web development, which is one
+        of the things I love about it. I am naturally creative, with an eye for
+        detail, and love buildings things. I am a quick learner, but most
+        importantly I am hardworking. I will do what it takes to learn the
+        skills necessary for any position. I am a dependable team player and
+        believe I would make a great addition to any development team.
       </p>
     </Fragment>
   );
