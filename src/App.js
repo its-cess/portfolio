bf6aaed3dc1456b0d65efd789b7e1cfb3114a51c
Cecily Toro <torocecily@gmail.com>
2022-10-05 21:51:31 -0400

    lock file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React from "react";
-import { Fragment } from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Navigation from "./navigation/Navigation";
-import LandingPage from "./components/pages/LandingPage";
-import AboutMe from "./components/pages/AboutMe";
-import Experience from "./components/pages/Experience";
-import Projects from "./components/pages/Projects";
-import Contact from "./components/pages/Contact";
-
 export default function App() {
   return (
-    <Fragment>
-      <Navigation />
-      <Routes basename="portfolio">
-        <Route path="portfolio" element={<LandingPage />} />
-        <Route path="portfolio/about-me" element={<AboutMe />} />
-        <Route path="portfolio/experience" element={<Experience />} />
-        <Route path="portfolio/projects" element={<Projects />} />
-        <Route path="portfolio/contact-me" element={<Contact />} />
-      </Routes>
-    </Fragment>
+    <div>
+      <h1>Hello!</h1>
+      My portfolio has been update and moved{" "}
+      <a href="https://www.cecilytoro.com/">here</a>!
+    </div>
   );
 }
